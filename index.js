a07c3097f4926f2d50b1d5dc85c10865d5b939f6
Steven Conner <stevenconner@Stevens-MacBook-Pro.local>
2017-12-20 19:04:17 -0800

    add logic to device.close() when process exits

--- index.js
+++ index.js
@@ -172,6 +172,32 @@ function keypadListener(send) {
   try {
     const device = new hid.HID(vendorId, productId);
     device.on('data', onKeypadData(send, device));
+    process.stdin.resume();//so the program will not close instantly
+
+    function exitHandler(options, err) {
+      if (options.cleanup) {
+        device.close();
+        console.log('clean')
+      };
+      if (err) console.log(err.stack);
+      if (options.exit) {
+        process.exit();
+      }
+    }
+
+    //do something when app is closing
+    process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+    // catches "kill pid" (for example: nodemon restart)
+    process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+    process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+    
   } catch (err) {
     console.log(err);
   }
